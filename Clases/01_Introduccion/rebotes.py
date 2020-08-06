47403e196f80f47c00ec780777281ab3b04fdc32
Leonardo Calderon <leogcalderon1994@gmail.com>
2020-08-06 11:29:00 -0300

    Modificado rebotes

--- Clases/01_Introduccion/rebotes.py
+++ Clases/01_Introduccion/rebotes.py
@@ -1,7 +1,3 @@
-'''Una pelota de goma es arrojada desde una altura de 100 metros
-y cada vez que toca el piso salta 3/5 de la altura desde la que cayó.
-Escribí un programa rebotes.py que imprima una tabla mostrando las alturas
-que alcanza en cada uno de sus primeros diez rebotes.'''
 
 altura = 100
 reduccion = 3/5
