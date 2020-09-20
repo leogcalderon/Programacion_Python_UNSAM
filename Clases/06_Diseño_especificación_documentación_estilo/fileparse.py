fb236d85464a21a56a5aa81cd1462917ecfb2ccf
Leonardo Calderon <leogcalderon1994@gmail.com>
2020-09-20 09:59:21 -0300

    Agrego duck tape

--- Clases/06_Diseño_especificación_documentación_estilo/fileparse.py
+++ Clases/06_Diseño_especificación_documentación_estilo/fileparse.py
@@ -1,6 +1,6 @@
 import csv
 
-def parse_csv(nombre_archivo, select = None, types = None, has_headers = True, silence_errors = True):
+def parse_csv(lines, select = None, types = None, has_headers = True, silence_errors = True):
     '''
     Parsea un archivo CSV en una lista de registros
     --------------------------------
@@ -16,52 +16,60 @@ def parse_csv(nombre_archivo, select = None, types = None, has_headers = True, s
         if not has_headers and select:
             raise RuntimeError("Para seleccionar, necesito encabezados.")
 
-    with open(nombre_archivo) as f:
-        rows = csv.reader(f)
-
-        if has_headers:
-            # Lee los encabezados
-            headers = next(rows)
-            records = []
-
-            #Indices seleccionados
-            if select:
-                idx = [headers.index(col) for col in select]
-                headers = select
-            else:
-                idx = []
-
-            for i,row in enumerate(rows):
-
-                '''Manejo de excepcion 2'''
-                try:
-                    if not row:    # Saltea filas sin datos
-                        continue
-                    if idx:        # Si hay indices seleccionados
-                        row = [row[i] for i in idx]
-
-                    if types:      # Si hay tipos especificados
-                        row = [func(val) for func,val in zip(types,row)]
-
-                    record = dict(zip(headers,row))
-                    records.append(record)
-
-                except ValueError as e:
-                    if not silence_errors:
-                        print(f'Row {i+1}: No pude convertir',row)
-                        print(f'Row {i+1}: Motivo:',e)
-                    else:
-                        pass
-
+    '''Duck taping'''
+    rows = []
+    for row in lines:
+        if row.endswith('\n'):
+            rows.append(row[:-1])
         else:
-            records = []
-            for row in rows:
-                if not row:
+            rows.append(row)
+
+    if has_headers:
+        # Lee los encabezados
+        headers = rows[0].split(',')
+        records = []
+
+        #Indices seleccionados
+        if select:
+            idx = [headers.index(col) for col in select]
+            headers = select
+        else:
+            idx = []
+
+        for i,row in enumerate(rows[1:]):
+
+            '''Manejo de excepcion 2'''
+            try:
+                if not row:    # Saltea filas sin datos
                     continue
-                if types:
+
+                row = row.split(',')
+
+                if idx:        # Si hay indices seleccionados
+                    row = [row[i] for i in idx]
+
+                if types:      # Si hay tipos especificados
                     row = [func(val) for func,val in zip(types,row)]
 
-                row = tuple(i for i in row)
-                records.append(row)
+                record = dict(zip(headers,row))
+                records.append(record)
+
+            except ValueError as e:
+                if not silence_errors:
+                    print(f'Row {i+1}: No pude convertir',row)
+                    print(f'Row {i+1}: Motivo:',e)
+                else:
+                    pass
+
+    else:
+        records = []
+        for row in rows:
+            if not row:
+                continue
+            if types:
+                row = [func(val) for func,val in zip(types,row)]
+
+            row = tuple(i for i in row)
+            records.append(row)
 
     return records
